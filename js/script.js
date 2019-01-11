8ad4346f73166416a62dcf5fc66d57b4e468f0ad
eliakilcher <36333192+eliakilcher@users.noreply.github.com>
2019-01-11 15:34:47 +0100

    bureger menu

--- js/script.js
+++ js/script.js
@@ -7,3 +7,12 @@ function parallax(){
     var scrolled = $(window).scrollTop();
     $('.header').css('top', +(scrolled * 0.3) + 'px');
 }
+
+
+/*** MENU ***/
+$(document).ready(function() {
+  $('.menu-button').click(function(event) {
+    $(".menu").toggleClass("menu-block");
+    $(".menu-button").toggleClass("backtooverview:hover");
+  });
+});
